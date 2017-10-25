821a2e51f3707788a67d32383838aa94754968ed
Chemley <canaowens@gmail.com>
2017-10-24 22:28:42 -0400

    health function working and changed some of the text for the game play.k

--- scriptsWithFunctionsAndMethods.js
+++ scriptsWithFunctionsAndMethods.js
@@ -1,5 +1,5 @@
 function game() {
-  var choice = prompt("Do you want to play? Type 'yes' or 'no.'");
+  var choice = prompt("Defeat The All Mighity Grant! Type 'yes' or 'no' to play!");
   if (choice.toLowerCase() === "yes") {
     var userName = prompt("What is your name?");
     console.log("Excellent! Welcome " + userName + "!");
@@ -16,8 +16,9 @@ function startCombat(playerName) {
 var user = {
   health: 40,
   heal: 2,
+  gamesWon: 0,
   playerAttackDamage: function(){
-    return (Math.floor(Math.random() * 5) + 1);
+    return (Math.floor(Math.random() * 3) + 1);
   },
   healing: function (){
     return (Math.floor(Math.random() * 10) + 1);
@@ -25,19 +26,16 @@ var user = {
 };
 
 var comp = {
-  name: "All Mighity Grant",
+  name: "The All Mighity Grant",
   lives: 10,
   grantAttackDamage: function(){
     return (Math.floor(Math.random() * 3) + 1);
   }
 };
 
-//var grantLives = 10;
-//var playerLives = 40;
-var gamesWon = 0;
 //While loop is nested in the game so it will loop only for the game.
 while (comp.lives > 0 && user.health > 0) {
-  var playGame = prompt("Do you want to attack or quit? Type 'attack' or 'quit.'");
+  var playGame = prompt("Do you want to attack or quit? Type 'attack,' 'heal,' or 'quit.'");
   if (playGame.toLowerCase() === "attack") {
       if (comp.lives >= 0){
         comp.lives -= comp.grantAttackDamage();
@@ -46,20 +44,23 @@ while (comp.lives > 0 && user.health > 0) {
         console.log(comp.name + " score is " + comp.lives);
       }
       if (comp.lives <= 0) {
-        gamesWon = gamesWon + 1;
-        console.log("You won " + gamesWon + " games!");
+        user.gamesWon = user.gamesWon + 1;
+        console.log("You won " + user.gamesWon + " games!");
         comp.lives +=10;
       }
-      if (gamesWon === 3) {
+      if (user.gamesWon === 5) {
         console.log("You defeated grant!");
         // To get it to stop running use null because the variable grantLives is looped to add 10 when it reaches zero. as a backup used break to stop the loop
         comp.lives = null;
         break;
       }
+      if (user.health <= 0 && user.gamesWon !== 5) {
+        console.log(comp.name + " is our winner. Better luck next time!")
+      }
   } else if (playGame.toLowerCase() === "heal") {
     if (user.heal > 0) {
-      user.heal = user.heal - 1;
-      user.health += user.healing();
+      user.heal -=1;
+      user.health +=user.healing();
       console.log("You can heal " + user.heal + " more time. Your new health is " + user.health + ".");
     } else {
       console.log("Use what you've got! There ain't none here.");
